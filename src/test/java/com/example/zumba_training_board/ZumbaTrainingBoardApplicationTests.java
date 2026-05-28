9efade28cdf97f8942cfd6d8115b3df8be36c2a9
Maria Timofejev <maria.timofejev.001@mail.ee>
2026-05-28 09:12:31 +0300

    Initial commit: Zumba training board backend

---
+++ src/test/java/com/example/zumba_training_board/ZumbaTrainingBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zumba_training_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZumbaTrainingBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
